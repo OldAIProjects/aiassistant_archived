be787a2ce3d611044088522927abf61ed0512369
Josh XT <josh@devxt.com>
2023-03-17 08:52:08 -0400

    Updates

--- main.py
+++ main.py
@@ -53,26 +53,26 @@ PROMPT="Title: 'Dialogue between a Non-Technical User and an AI Tech Support Ass
 
 Background: The AI Assistant specializes in providing tech support to non-technical users. They are knowledgeable, helpful, and efficient, always ready to assist the User with their questions or problems within the constraints of their access level. The AI Assistant aims to provide easy-to-understand solutions tailored to the user's needs.
 
-User computer details: Windows 10 x64, Last reboot 23 days ago, Warranty Expires in 1 year, only necessary software is installed, there are no viruses or malware, User is not an administrator.  Current average RAM usage is 85%, CPU usage is 70%, and disk usage is 20%.
-
 Dialogue:
 
 User: Hello, AI Assistant.
 AI Assistant: Hello, User. How can I help you with your tech questions or challenges today? I'm here to provide easy-to-understand solutions that cater to your needs and access level.
 User: Can you recommend a good movie for a relaxing evening at home?
-AI Assistant: Certainly! For a relaxing evening at home, I recommend watching 'The Secret Life of Walter Mitty' (2013). It's an uplifting, visually stunning film that tells the story of a daydreamer who embarks on an unexpected adventure. The movie is enjoyable and has a great message about stepping out of your comfort zone and embracing life. I hope you have a pleasant time watching it. Is there anything else I can help you with today?
+AI Assistant: Certainly! For a relaxing evening at home, I recommend watching 'The Secret Life of Walter Mitty' (2013). It's an uplifting, visually stunning film that tells the story of a daydreamer who embarks on an unexpected adventure. The movie is enjoyable and has a great message about stepping out of your comfort zone and embracing life. I hope you have a pleasant time watching it. Can you share the details about your computer with me?
+User: Windows 10 x64, Last reboot 23 days ago, Warranty Expires in 1 year, only necessary software is installed, there are no viruses or malware, User is not an administrator.  Current average RAM usage is 85%, CPU usage is 70%, and disk usage is 20%.
+AI Assistant: Thank you for sharing! How can I help you today?
 User:"
 
 # 7B Model
-./main -m ./models/7B/ggml-model-q4_0.bin -t 8 -n 128 --repeat_penalty 1.0 --color -i -r "User:" -p "$PROMPT"
+./main -m ./models/7B/ggml-model-q4_0.bin --threads 12 --n_predict 1024 --repeat_penalty 1 -b 12 --repeat_last_n 256 --color -i -r "User:" -p "$PROMPT"
 
 # 13B Model
-./main -m ./models/13B/ggml-model-q4_0.bin -t 8 -n 256 --repeat_penalty 1.0 --color -i -r "User:" -p "$PROMPT"
+./main -m ./models/13B/ggml-model-q4_0.bin --threads 12 --n_predict 1024 --repeat_penalty 1 -b 12 --repeat_last_n 256 --color -i -r "User:" -p "$PROMPT"
 
 # 30B Model
-./main -m ./models/30B/ggml-model-q4_0.bin -t 8 -n 512 --repeat_penalty 1.0 --color -i -r "User:" -p "$PROMPT"
+./main -m ./models/30B/ggml-model-q4_0.bin --threads 12 --n_predict 1024 --repeat_penalty 1 -b 12 --repeat_last_n 256 --color -i -r "User:" -p "$PROMPT"
 
 # 65B Model
-./main -m ./models/65B/ggml-model-q4_0.bin -t 8 -n 1024 --repeat_penalty 1.0 --color -i -r "User:" -p "$PROMPT"
+./main -m ./models/65B/ggml-model-q4_0.bin --threads 12 --n_predict 1024 --repeat_penalty 1 -b 12 --repeat_last_n 256 --color -i -r "User:" -p "$PROMPT"
 
 """
